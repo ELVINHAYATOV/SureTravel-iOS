89ae1718c25573350f7fb364ec0a5368df529728
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2024-03-01 19:10:53 +0400

    Initial Commit

---
+++ SureTravel-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SureTravel-iOS
+//
+//  Created by Alvin on 01.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
