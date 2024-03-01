d6176e4e7d23459dc0ea43a42d86b2592d3662c5
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2024-03-01 22:15:44 +0400

    Adding Progress view

--- SureTravel-iOS/ViewController.swift
+++ SureTravel-iOS/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let transform : CGAffineTransform =  CGAffineTransformMakeScale(1.0, 3.0)
+        progressBar.transform = transform
     }
-
-
 }
 
