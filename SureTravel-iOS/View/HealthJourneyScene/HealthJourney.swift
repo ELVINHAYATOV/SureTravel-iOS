fe49ee1d6bf8a574fde4bd9cbc3cef627908b52b
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2024-03-02 15:22:09 +0400

    Adding elements to Health scene

--- SureTravel-iOS/View/HealthJourneyScene/HealthJourney.swift
+++ SureTravel-iOS/View/HealthJourneyScene/HealthJourney.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class HealthJourney: GradientViewController {
+    @IBOutlet weak var motivationalLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     private let healthJourneyTitleString = NSLocalizedString("HEALTH_JOURNEY_TITLE", comment: "")
+    private let motivationalString = NSLocalizedString("HEALTH_MOTIVATION", comment: "")
+    private let metricsString = NSLocalizedString("HEALTH_VITAL_METRICS", comment: "")
+    
+    private let userDate = UserDataSource.getUserData()
+    
+    func setJourney(){
+        let calendar = Calendar.current
+        
+        if let startDate = calendar.date(from: self.userDate.getDate()),
+           let endDate = calendar.date(byAdding: .year, value: 1, to: startDate) {
+            
+            let pastDays = calendar.dateComponents([.day], from: startDate, to: Date()).day ?? 0
+            
+            let totalDays = calendar.dateComponents([.day], from: startDate, to: endDate).day ?? 0
+            
+            resultLabel.text = "Trt to be healty for another \(totalDays - pastDays) days."
+            progressBar.progress = Float(pastDays) / Float(totalDays)
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = healthJourneyTitleString
+        self.motivationalLabel.text = motivationalString
+        
+        setJourney()
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+        self.navigationItem.titleView?.tintColor = .white
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.barStyle = .black
     }
+    
 }
 
